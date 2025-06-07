8adbb0ad22fb35d218c43c88e441217de6e7b0ba
Harsh Patil <itsharshhh6@gmail.com>
2025-06-07 17:41:26 +0530

    Refactor MyThread logic to improve thread behavior
    Revised the run() method to include a loop with incremental logging and adjusted sleep duration. Simplified the main function for cleaner thread initialization and termination output.

--- src/MyThread.java
+++ src/MyThread.java
@@ -1,22 +1,20 @@
 public class MyThread extends Thread{
     @Override
     public void run() {
-        System.out.println("RUNNING");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        for (int i = 0; i < 4; i++){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i);
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        MyThread t1 = new MyThread();
-        System.out.println(t1.getState());
-        t1.start();
-        System.out.println(t1.getState());
-        Thread.sleep(100);
-        System.out.println(t1.getState());
-        t1.join();
-        System.out.println(t1.getState());
+        Thread thread = new MyThread();
+        thread.start();
+        thread.join();
+        System.out.println("end");
     }
 }
