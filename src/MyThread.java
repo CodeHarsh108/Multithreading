50504c7e117cea5df279668d9f6da36823a741d1
Harsh Patil <itsharshhh6@gmail.com>
2025-06-14 15:49:53 +0530

    Setting priority of threads

--- src/MyThread.java
+++ src/MyThread.java
@@ -1,20 +1,32 @@
 public class MyThread extends Thread{
+    public MyThread(String name){
+        super(name);
+    }
     @Override
     public void run() {
-        for (int i = 0; i < 4; i++){
+        for (int i = 0; i < 5; i++){
+            String a = " ";
+            for(int j = 0; j < 100000; j++){
+                a += "a";
+            }
+            System.out.println(Thread.currentThread().getName() + " Priority : " + Thread.currentThread().getPriority() + " count " + i);
             try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
+                Thread.sleep(100);
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-            System.out.println(i);
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new MyThread();
-        thread.start();
-        thread.join();
-        System.out.println("end");
+        Thread l = new MyThread("Low");
+        Thread m = new Thread("Medium");
+        Thread h = new Thread("High");
+        l.setPriority(MIN_PRIORITY);
+        m.setPriority(NORM_PRIORITY);
+        h.setPriority(MAX_PRIORITY);
+        l.start();
+        m.start();
+        h.start();
     }
 }
