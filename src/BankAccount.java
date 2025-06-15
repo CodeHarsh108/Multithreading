6f2b62b2e99cbe74e34cf148028b4ec7bd03af29
Harsh Patil <itsharshhh6@gmail.com>
2025-06-15 15:09:29 +0530

    Using Lock

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,14 +1,28 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
     private int balance = 100;
-    public synchronized void withdraw(int amount) throws InterruptedException {
+    private final Lock lock = new ReentrantLock();
+    public  void withdraw(int amount) throws InterruptedException {
         System.out.println(Thread.currentThread().getName() + " attempting to withdraw " + amount);
-        if (balance >= amount){
-            System.out.println(Thread.currentThread().getName() + " proceeding with withdrawal ");
-            Thread.sleep(10000);
-            balance -= amount;
-            System.out.println(Thread.currentThread().getName() + " completed withdrawal, Remaining balance : " + balance);
+        if(lock.tryLock(1000, TimeUnit.MILLISECONDS)){
+            if (balance >= amount){
+                try{
+                System.out.println(Thread.currentThread().getName() + " processing");
+                Thread.sleep(3000);
+                balance -= amount;
+                System.out.println(Thread.currentThread().getName() + " completed");} catch (Exception e) {
+                    throw new RuntimeException(e);
+                }finally {
+                    lock.unlock();
+                }
+            }else{
+                System.out.println(Thread.currentThread().getName() + " insufficient balance");
+            }
         }else{
-            System.out.println(Thread.currentThread().getName() + " insufficient balance ");
+            System.out.println(Thread.currentThread().getName() + " try later");
         }
     }
 }
