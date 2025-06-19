1813ec6bea9c15adeb8c7fa3265e10cfa860a181
Harsh Patil <itsharshhh6@gmail.com>
2025-06-19 16:26:00 +0530

    Factorial method

--- src/executorframework/Main.java
+++ src/executorframework/Main.java
@@ -1,8 +1,16 @@
 package executorframework;
 
 public class Main {
-
+    public static long factorial(long n){
+        long result = 1;
+        for(int i = 1; i <=n; i++){
+            result *= i;
+        }
+        return result;
+    }
     public static void main(String[] args) {
-
+        for (long i = 1; i < 10; i++) {
+            System.out.println(factorial(i));
+        }
     }
 }
