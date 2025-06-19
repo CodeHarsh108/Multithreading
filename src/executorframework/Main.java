5e0d9260f7989a5ae1aa00d1e3622fc5dc5690dd
Harsh Patil <itsharshhh6@gmail.com>
2025-06-19 17:05:04 +0530

    Factorial method with intervals, with time counted but now using individual threads and reducing time

--- src/executorframework/Main.java
+++ src/executorframework/Main.java
@@ -15,8 +15,24 @@ public class Main {
     }
     public static void main(String[] args)  {
         long start = System.currentTimeMillis();
+        Thread[] threads = new Thread[9];
         for (long i = 1; i < 10; i++) {
-            System.out.println(factorial(i));
+            long finalI = i;
+            threads[(int) (i-1)] = new Thread(
+                    () -> {
+                        long result = factorial(finalI);
+                        System.out.println(result);
+                    }
+            );
+            threads[(int) (i-1)].start();
+
+        }
+        for(Thread thread : threads){
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
         System.out.println("Total time : " + (System.currentTimeMillis() - start));
     }
