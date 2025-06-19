dd902473b7384debdb983573ce1c96e5f0a6f593
Harsh Patil <itsharshhh6@gmail.com>
2025-06-19 16:33:30 +0530

    Factorial method with intervals, with time counted

--- src/executorframework/Main.java
+++ src/executorframework/Main.java
@@ -2,16 +2,22 @@ package executorframework;
 
 public class Main {
     public static long factorial(long n){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         long result = 1;
         for(int i = 1; i <=n; i++){
             result *= i;
         }
         return result;
     }
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
+        long start = System.currentTimeMillis();
         for (long i = 1; i < 10; i++) {
-            Thread.sleep(1000);
             System.out.println(factorial(i));
         }
+        System.out.println("Total time : " + (System.currentTimeMillis() - start));
     }
 }
