756ecfa9aaefb6a6543defac3abb485f9c81a1c2
Harsh Patil <itsharshhh6@gmail.com>
2025-11-29 11:07:53 +0530

    Yield()

---
+++ src/Methods/Yield.java
@@ -0,0 +1,22 @@
+package Methods;
+
+public class Yield {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Thread 1 : " + i);
+                Thread.yield();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Thread 2 : " + i);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+    }
+}
