aa0ea6f0e291a6be563debde27c0da6984e9189a
Harsh Patil <itsharshhh6@gmail.com>
2025-11-30 20:58:32 +0530

    Binary Semaphore

---
+++ src/Semaphores/BinarySemaphore.java
@@ -0,0 +1,23 @@
+package Semaphores;
+
+import java.util.concurrent.Semaphore;
+
+public class BinarySemaphore {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(1);
+        Runnable task = () -> {
+            try{
+                semaphore.acquire();
+                System.out.println(Thread.currentThread().getName() + "acquired");
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + "leaving");
+                semaphore.release();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+        for(int i = 1; i <= 5; i++){
+            new Thread(task, "T"+i).start();
+        }
+    }
+}
