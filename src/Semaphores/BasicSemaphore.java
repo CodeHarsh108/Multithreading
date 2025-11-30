154385b1f04c99ac023a4ef8eb398301c4b83c70
Harsh Patil <itsharshhh6@gmail.com>
2025-11-30 20:55:36 +0530

    Basic Semaphore

---
+++ src/Semaphores/BasicSemaphore.java
@@ -0,0 +1,23 @@
+package Semaphores;
+
+import java.util.concurrent.Semaphore;
+
+public class BasicSemaphore {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(2);
+        Runnable task = () -> {
+            try{
+                semaphore.acquire();
+                System.out.println(Thread.currentThread().getName() + "acquired");
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + "leaving");
+                semaphore.release();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+        for(int i = 1; i <= 5; i++){
+            new Thread(task, "T"+i).start();
+        }
+    }
+}
