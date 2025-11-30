84ab39f6abd3f0088e9c506b1f4b3a79d106fccb
Harsh Patil <itsharshhh6@gmail.com>
2025-11-30 18:45:52 +0530

    InvokeAll

---
+++ src/executorsframeworknew/InvokeAll.java
@@ -0,0 +1,22 @@
+package executorsframeworknew;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class InvokeAll {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        Callable<String> t1 = () -> "Task 1 done";
+        Callable<String> t2 = () -> "Task 2 done";
+        Callable<String> t3 = () -> "Task 3 done";
+        List<Callable<String>> tasks = Arrays.asList(t1, t2, t3);
+        List<Future<String>> results = service.invokeAll(tasks);
+        for(Future<String> f : results){
+            System.out.println(f.get());
+        }
+        service.shutdown();
+
+
+    }
+}
